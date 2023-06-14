dc6fcb1319c8909b6037eace1bb9668b01f3115a
lomin <lo.miniuk@outlook.com>
2023-06-14 20:45:37 +0300

    Section 6

--- Edenthought/journal/views.py
+++ Edenthought/journal/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+
+clientList = [
+    {
+        'id': '1',
+        'name': 'Bobby Brown',
+        'profession': 'Drawer',
+    },{
+        'id': '2',
+        'name': 'Sarah',
+        'profession': 'Carpenter',
+    },
+]
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def home(request):
+    context = {'clients': clientList}
+    return render(request, 'index.html',context)
+
